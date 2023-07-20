ac24e7a2fed12b4ee95307652f16414b50852977
weaverswebsubham <subham.das@weavers-web.com>
2023-07-20 12:08:54 +0530

    working on the navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
 
 function App() {
-
   return (
-    <>
-    <h1>Hello</h1>
-    </>
-  )
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
